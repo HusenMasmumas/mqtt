b850baa15481ea6d52f8cc789c09afbdeb45d39c
HusenMasmumas <sen.masmumas@gmail.com>
2023-07-18 12:02:13 +0700

    parameters ping

--- controllers/ping.js
+++ controllers/ping.js
@@ -1,33 +1,37 @@
 const exec = require("child_process").exec;
+const url = require("url");
 
 const location = "192.168.1.27";
 
-const pingFunction = async function () {
+const pingFunction = async function (url) {
   const promises = [];
+  try {
+    promises.push(
+      await new Promise((resolve) => {
+        exec(`ping -c 3 -W 1000 ${url || location}`, (err, stdout) => {
+          let status = "offline";
+          let output = stdout.toString();
+          let replyFromIndex = output.indexOf("from");
 
-  promises.push(
-    await new Promise((resolve, reject) => {
-      exec(`ping -c 3 -W 1000 ${location}`, (err, stdout, stderr) => {
-        let status = "offline";
-        let output = stdout.toString();
-        let replyFromIndex = output.indexOf("from");
-
-        if (
-          replyFromIndex > 0 &&
-          output.substring(replyFromIndex).toUpperCase().indexOf("BYTES") > 0
-        ) {
-          status = "online";
-        }
-        resolve({
-          date: new Date().toISOString(),
-          location: location,
-          status: status,
+          if (
+            replyFromIndex > 0 &&
+            output.substring(replyFromIndex).toUpperCase().indexOf("BYTES") > 0
+          ) {
+            status = "online";
+          }
+          resolve({
+            date: new Date().toISOString(),
+            location: url || location,
+            status: status,
+          });
         });
-      });
-    })
-  );
+      })
+    );
 
-  return promises;
+    return promises;
+  } catch {
+    return [];
+  }
 };
 
 setInterval(async function () {
@@ -40,11 +44,20 @@ setInterval(async function () {
 
 exports.getPingPage = async function (req, res) {
   try {
-    const promises = await pingFunction();
-    await res.status(200).json({
-      status: 200,
-      data: promises,
-    });
+    var url_parts = url.parse(req?.url, true);
+    var query = url_parts?.query;
+
+    if (!query.url) {
+      await res
+        .status(400)
+        .json({ status: 400, message: "Error request parameters url" });
+    } else {
+      const promises = await pingFunction(query?.url);
+      await res.status(200).json({
+        status: 200,
+        data: promises,
+      });
+    }
   } catch (error) {
     res
       .status(400)
